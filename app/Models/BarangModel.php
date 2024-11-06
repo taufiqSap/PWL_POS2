c25483c51f97641e7f57b5f00c30588f99219e82
taufiqSap <taufiqsatriaji@gmail.com>
2024-11-06 16:54:44 +0700

    JS 11 & TUGAS

--- app/Models/BarangModel.php
+++ app/Models/BarangModel.php
@@ -1,21 +1,37 @@
 <?php
+
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class BarangModel extends Model
 {
     use HasFactory;
+
     protected $table = 'm_barang'; // Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'barang_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['kategori_id','barang_kode','barang_nama', 'harga_beli', 'harga_jual'];
+
+    protected $fillable = ['kategori_id','barang_kode','barang_nama', 'harga_beli', 'harga_jual', 'image'];
+
     public function kategori():BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id','kategori_id');
     }
     public function barang():HasMany
     {
-        return $this->hasMany(BarangModel::class, 'barang_id', 'barang_id');
+        return $this->hasMany(StockModel::class, 'stock_id', 'stock_id');
+    }
+    public function detail():HasMany{
+        return $this->hasMany(DetailModel::class, 'barang_id', 'barang_id');
+    }
+
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image),
+        );
     }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,53 +2,65 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable;  //implementasi class authenticatable
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
 
-    public function getJWTIdentifier(){
+    public function getJWTIdentifier()
+    {
         return $this->getKey();
     }
 
-    public function getJWTCustomClaims(){
+    public function getJWTCustomClaims()
+    {
         return [];
     }
+
     use HasFactory;
 
-    protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //Mendefinisikan primary key dari tabel yang digunakan
+    protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'image','created_at', 'updated_at'];
 
-    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
+    protected $hiidden = ['password'];
 
-    protected $hidden = ['password']; // jangan ditampilkan saat select
+    protected $casts = ['password' => 'hashed'];
 
-    protected $casts = ['password' => 'hashed']; // casting password agar otomatis dihash 
 
-    // relasi ke tabel level
-    public function level(): BelongsTo {
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+    public function barang(): HasMany
+    {
+        return $this->hasMany(StockModel::class, 'stock_id', 'stock_id');
+    }
 
-    // mendapatkan nama role
+    protected function foto(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/'.$image),
+        );
+    }
 
-    public function getRoleName(): String {
+    public function getRoleName(): string
+    {
         return $this->level->level_nama;
     }
 
-    // cek apakah user memiliki role tertentu
-
-    public function hasRole($role): bool {
+    public function hasRole($role): bool
+    {
         return $this->level->level_kode == $role;
     }
 
-    // mendapatkan kode role
-
-    public function getRole() {
+    public function getRole()
+    {
         return $this->level->level_kode;
     }
 }
